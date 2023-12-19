2d2a2ecca9aafbf11c5daf86b390302641ffb300
Joao Ferreira <joaomateusferr@gmail.com>
2023-12-19 10:36:13 -0300

    Disable time limit

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+    set_time_limit(0);
+
     $GLOBALS['CookiePath'] = dirname(__FILE__).'/../cookie/content.txt';
     $GLOBALS['UserAgent'] = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:35.0) Gecko/20100101 Firefox/35.0';
     $GLOBALS['CsvExportPath'] = '/tmp/';
